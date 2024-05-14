c65412e55a8a2f8a6db187122523a0d96d57f516
Nitinyadav14920 <nitinkumar94555206@gmail.com>
2024-05-14 18:19:41 +0530

    postApp

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+
+import { Container, PostForm } from '../components'
+
+function AddPost() {
+    return (
+        <div className='py-8'>
+            <Container>
+                <PostForm />
+            </Container>
+        </div>
+    )
+}
+
+export default AddPost
