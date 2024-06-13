ed0ac444b73adb5b2ff2afd644c020609e00cf8f
Nitinyadav14920 <nitinkumar94555206@gmail.com>
2024-06-13 21:28:14 +0530

    Blogging app

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -8,13 +8,13 @@ function Select({
 }, ref) {
     const id = useId()
     return (
-        <div className=' absolute top-10 mt-24 w-full flex flex-auto gap-3 z-0 md:flex-wrap sm:flex-wrap min-[10px]:flex-wrap  '>
+        <div className='w-full'>
             {label && <label htmlFor={id} className=''></label>}
             <select
                 {...props}
                 id={id}
                 ref={ref}
-                className={`px-3 py-2 mt-10 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             >
                 {options?.map((option) => (
                     <option key={option} value={option}>
